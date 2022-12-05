ae93336b411e099fe882c775b1a9f11c24cef4ea
Luong <luong.hoangk20@hcmut.edu.vn>
2022-12-06 01:11:46 +0700

    update header and footer

---
+++ app/views/customer/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link rel="stylesheet" href="../../../public/css/header.css">
+  <link rel="stylesheet" href="../../../public/css/footer.css">
+  <link rel="stylesheet" href="../../../public/css/account/account.css">
+  <link rel="stylesheet" href="../../../public/css/account/sidebar.css">
+   
+  <script src="https://kit.fontawesome.com/9d371022aa.js" crossorigin="anonymous"></script>
+  <title>Home</title>
+</head>
+<body>
+  <?php
+    include '../components/header.php';
+  ?>
+  <?php
+    include '../components/footer.php';
+  ?>
+</body>
+</html>
