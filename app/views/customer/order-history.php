f624202c70a7ef63379a837d894bc6048254eff0
Hoang Nhat Ha <ha.hoang3788ha@hcmut.edu.vn>
2022-12-06 02:55:28 +0700

    Add order detail

--- app/views/customer/order-history.php
+++ app/views/customer/order-history.php
@@ -6,12 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bookstore</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="../../js/jquery.js"></script>
     <link rel="stylesheet" href="../../../public/css/header.css">
     <link rel="stylesheet" href="../../../public/css/footer.css">
     <link rel="stylesheet" href="../../../public/css/account/sidebar.css">
     <link rel="stylesheet" href="../../../public/css/account/order-history.css">
-    <script src="../../js/function.js"></script>
     <script src="https://kit.fontawesome.com/9d371022aa.js" crossorigin="anonymous"></script>
 </head>
 <body>
@@ -46,7 +44,7 @@
                                         <td>100.000</td>
                                         <td>
                                             <div class="action">
-                                            <button type="button" class="btn btn-primary">Chi tiết</button>
+                                           <a href="order-detail.php"> <button type="button" class="btn btn-primary">Chi tiết</button></a>
                                             </div>
                                         </td>
                                         </tr>
@@ -74,7 +72,7 @@
                                             <td>100.000</td>
                                             <td>
                                                 <div class="action">
-                                                <button type="button" class="btn btn-primary">Chi tiết</button>
+                                                <a href="order-detail.php"> <button type="button" class="btn btn-primary">Chi tiết</button></a>
                                             </div>
                                             </td>
                                             </tr>
@@ -89,5 +87,7 @@
     </div>
     <?php include '../components/footer.php';?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<script src="../../js/jquery.js"></script>
+<script src="../../js/function.js"></script>
 </body>
 </html>
